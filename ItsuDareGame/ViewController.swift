71e6785ac651f3508fbb70356990b4a20338778c
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2022-02-09 03:55:57 +0900

    いつだれゲームDone!!

--- ItsuDareGame/ViewController.swift
+++ ItsuDareGame/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var itsuLabel: UILabel!
+    @IBOutlet var dokodeLabel: UILabel!
+    @IBOutlet var daregaLabel: UILabel!
+    @IBOutlet var doshitaLabel: UILabel!
+    
+    let itsuArrey: [String] = ["一年前","一週間","昨日","今日"]
+    let dokodeArrey: [String] = ["山の上で","アメリカで","学校で","クラスで"]
+    let daregaArrey: [String] = ["僕が","大統領が","先生が","友達が"]
+    let doshitaArrey: [String] = ["叫んだ","演説した","怒った","踊った"]
+    
+    var index: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func change(){
+        itsuLabel.text = itsuArrey[index]
+        dokodeLabel.text = dokodeArrey[index]
+        daregaLabel.text = daregaArrey[index]
+        doshitaLabel.text = doshitaArrey[index]
+        
+        index = index + 1
+        
+        if index > 3{
+            index = 0
+        }
+    }
 
-
+    @IBAction func reset(){
+        itsuLabel.text = "-----"
+        dokodeLabel.text = "-----"
+        daregaLabel.text = "-----"
+        doshitaLabel.text = "-----"
+        
+        index = 0
+    }
+    
+    @IBAction func random(){
+        let itsuIndex = Int.random(in: 0...3)
+        let dokodeIndex = Int.random(in: 0...3)
+        let daregaIndex = Int.random(in: 0...3)
+        let doshitaIndex = Int.random(in: 0...3)
+        
+        itsuLabel.text = itsuArrey[itsuIndex]
+        dokodeLabel.text = dokodeArrey[dokodeIndex]
+        daregaLabel.text = daregaArrey[daregaIndex]
+        doshitaLabel.text = doshitaArrey[doshitaIndex]
+    }
+    
 }
 
